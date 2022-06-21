cb36f93db7514c912bc2bb8897aa3671fbd1fb9b
Lynn <48946984+Lynnsanee@users.noreply.github.com>
2022-06-21 14:06:47 +0200

    Add config template

---
+++ cell_parameters.py
@@ -0,0 +1,16 @@
+
+# cell: NR02-GOLF-12
+intelmap_lat = "51.612244836505255"
+intelmap_lng = "7.146387405329828"
+latE6 = "51612245"
+lngE6 = "7146387"
+
+# Telegram information
+bot = "BOT_TOKEN"
+chatid = 0 # chatid, most channels have -100 as a prefix
+
+# Ingress Authentication
+csrf = "CSRF_HERE"
+sessionid = "SESSIONID_HERE"
+v = "V_HERE"
+
